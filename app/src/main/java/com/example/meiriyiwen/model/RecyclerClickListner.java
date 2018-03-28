a27e147a3a5d06b07a22d1c92e81a114c4269733
limubai1998@gmail.com <limubai1998@gmail.com>
2018-03-29 00:46:17 +0800

    目录详情页完成，点击加载更多完成
    目录详情页完成，点击加载更多完成

--- app/src/main/java/com/example/meiriyiwen/model/RecyclerClickListner.java
+++ app/src/main/java/com/example/meiriyiwen/model/RecyclerClickListner.java
@@ -1,14 +1,9 @@
 package com.example.meiriyiwen.model;
 
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-
-import com.example.meiriyiwen.presenter.BookShelfAdapter;
-
 /**
  * Created by 李木白 on 2018/3/25.
  */
 
 public interface RecyclerClickListner<type> {
-    public abstract void onClickListner(type viewHolder, int position);
+    void onClickListner(type viewHolder, int position);
 }
